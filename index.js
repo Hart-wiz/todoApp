cd015c87923ae5d146152fe741ac54bc8b2536c2
njimogu wisdom <njimoguwisdom@gmail.com>
2024-12-02 11:58:22 +0100

    committing aan new todo list better version

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const form = document.querySelector("form");
 const textarea = document.querySelector(".textarea");
+const timearea = document.querySelector(".time");
 const main = document.querySelector("main");
 
 function createCard() {
   const text = textarea.value;
+  const time = timearea.value;
   const cardBody = document.createElement("div");
 
   main.appendChild(cardBody);
-  card.classList.add("card");
+  cardBody.classList.add("card");
 
-  card.innerText = text;
+  const todoText = document.createElement("article");
+  cardBody.appendChild(todoText);
+  todoText.classList.add("textClass");
+
+  const todoTime = document.createElement("p");
+  cardBody.appendChild(todoTime);
+
+  todoText.innerHTML = text;
+  todoTime.innerText = time;
   form.classList.add("not-visible");
+
+  // creating the delete btn
+
+  function handleClick() {
+    alert("clicked button");
+  }
+  const createDelBtn = document.createElement("button");
+  createDelBtn.innerHTML = "delete";
+  cardBody.appendChild(createDelBtn);
+  createDelBtn.classList.add("del-btn");
+  createDelBtn.setAttribute("onclick", handleClick());
+  // cardBody.classList.remove("card");
+  // cardBody.classList.add("not-visible");
+
+  // creating the completed task btn
+  const completedTaskBtn = document.createElement("button");
 }
 
 function addTodo() {
