deb010ae6641842314cda03ce16fa9bf87be5915
njimogu wisdom <njimoguwisdom@gmail.com>
2024-12-02 23:11:24 +0100

    version 0.one complete

--- index.js
+++ index.js
@@ -1,46 +1,28 @@
-const form = document.querySelector("form");
-const textarea = document.querySelector(".textarea");
-const timearea = document.querySelector(".time");
-const main = document.querySelector("main");
+const listContainer = document.querySelector(".list-container");
+const inputBox = document.querySelector(".input-box");
 
-function createCard() {
-  const text = textarea.value;
-  const time = timearea.value;
-  const cardBody = document.createElement("div");
-
-  main.appendChild(cardBody);
-  cardBody.classList.add("card");
-
-  const todoText = document.createElement("article");
-  cardBody.appendChild(todoText);
-  todoText.classList.add("textClass");
-
-  const todoTime = document.createElement("p");
-  cardBody.appendChild(todoTime);
-
-  todoText.innerHTML = text;
-  todoTime.innerText = time;
-  form.classList.add("not-visible");
-
-  // creating the delete btn
-
-  function handleClick() {
-    alert("clicked button");
+function addTask() {
+  if (inputBox.value === "") {
+    alert("type in a task");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "x";
+    li.appendChild(span);
   }
-  const createDelBtn = document.createElement("button");
-  createDelBtn.innerHTML = "delete";
-  cardBody.appendChild(createDelBtn);
-  createDelBtn.classList.add("del-btn");
-  createDelBtn.setAttribute("onclick", handleClick());
-  // cardBody.classList.remove("card");
-  // cardBody.classList.add("not-visible");
-
-  // creating the completed task btn
-  const completedTaskBtn = document.createElement("button");
+  inputBox.value = "";
 }
 
-function addTodo() {
-  form.classList.add("visible");
-  form.classList.remove("not-visible");
-  textarea.value = "";
-}
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove("LI");
+    }
+  },
+  false
+);
